b209f0effde8dc0c4002db72baef7d947a0be197
pinkipi <yarnovax@gmail.com>
2019-11-08 11:02:53 -0500

    Command rewrite

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# broker-anywhere
-tera-proxy module to open the broker anywhere
-
 ## Usage
-`!broker` in chat
+Type `/!broker` in chat.
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-module.exports = function BrokerAnywhere(dispatch){
-    const chatHook = event => {
-        if(!event.message.toLowerCase().includes('!broker')) return
-        
-        dispatch.toClient('S_NPC_MENU_SELECT', 1, {type:28})
-        return false
-    }
-    dispatch.hook('C_CHAT', 1, chatHook)
-    dispatch.hook('C_WHISPER', 1, chatHook)
+module.exports = function BrokerAnywhere(mod) {
+	const { command } = mod.require
+
+	command.add('broker', () => { mod.send('S_NPC_MENU_SELECT', 1, { type: 28 }) })
 }
